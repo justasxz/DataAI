bbc08659c9945941c5af04995529f88986f4527f
Justas Kvederis <justas@computech.ie>
2026-02-25 12:24:54 +0200

    dataframes

--- app.py
+++ app.py
@@ -101,6 +101,55 @@ import numpy as np
 # # print(data_with_nan)
 # data_with_nan = data_with_nan.map({np.nan: 0, 1: 10, 2: 20, 3: 30, 5: 50, 6: 60, 8: 80, 9: 90, 10: 100, 11: 110, 12: 120, 13: 130, 14: 140, 15: 150}) # map - pakeicia reiksmes pagal nurodyta zodyna, np.nan: 0 - pakeicia null reiksmes i 0
 # print(data_with_nan)
-unsorted_series_with_nan = pd.Series([5, 2, 3, np.nan, 1, 4, np.nan])
-sorted_series = unsorted_series_with_nan.sort_values() # sort_values - surusiuoja
-print(sorted_series)
+# unsorted_series_with_nan = pd.Series([5, 2, 3, np.nan, 1, 4, np.nan])
+# sorted_series = unsorted_series_with_nan.sort_values() # sort_values - surusiuoja
+# print(sorted_series)
+
+df = pd.DataFrame({
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve', np.nan, "Jonas"],
+    'Age': [25, 30, 35, 40, 45, 50,np.nan],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio'],
+    'Salary': [50000, 60000, 70000, 80000, 90000, 100000, 300000],
+    'Department': [np.nan, "HR", np.nan, np.nan, "IT", np.nan, np.nan]
+})
+
+# print(df)
+# print(df.shape)
+# df.info()
+# print(df.describe()) # describe - pateikia statistines reiksmes apie skaitinius stulpelius, count - kiek yra ne null reiksmiu, mean - vidurkis, std - standartinis nuokrypis, min - minimumas, 25% - pirmasis kvartilis, 50% - antrasis kvartilis (mediana), 75% - treciasis kvartilis, max - maksimumas
+# print(df['Salary'].quantile(0.25,interpolation='nearest')) # quantile - pateikia nurodyta kvartili, interpolation - nurodo kaip apskaiciuoti kvartili, lower - naudoja apacia reiksme, higher - naudoja virsutine reiksme, midpoint - naudoja vidurki tarp apacios ir virsutines reiksmes
+# print(df.describe(include='all')) # include - nurodo kokio tipo stulpelius apibrezti, object - tekstiniai stulpeliai, all - visi stulpeliai
+# print(df['Name'])
+
+# print(df.describe()['Age'])
+
+# print(df[['Name', 'City']]) # pasiekiama kelis stulpelius pagal pavadinima
+# filter
+# print(df[df['Age'] > 30]) # isfiltruojami eilutes, kuriose amzius didesnis uz 30
+# rename
+# df.rename(columns={'Name': 'Full Name', 'Age': 'Age in Years'}, inplace=True) # rename - pervadina stulpelius, columns - nurodo kuriuos stulpelius pervadinti, inplace - pakeicia originala, jei False - grazina nauja DataFrame
+# print(df)
+# print(df.isnull())
+# df['Age'] = df['Age'].fillna(100) # fillna - uzpildo null reiksmes nurodytu skaiciu
+# print(df)
+# df.fillna({'Name': 'Unknown', 'Age': 100}, inplace=True) # fillna - uzpildo null reiksmes nurodytu skaiciu, galima nurodyti skirtingas reiksmes skirtingiems stulpeliams
+# print(df)
+# df.dropna(inplace=True,thresh=3) # dropna - pasalina eilutes su null reiksmemis, inplace - pakeicia originala, jei False - grazina nauja DataFrame
+# print(df)
+# df.dropna(axis=1, thresh=4, inplace=True) # dropna - pasalina stulpelius su null reiksmemis, axis - nurodo ar pasalinti eilutes (axis=0) ar stulpelius (axis=1), inplace - pakeicia originala, jei False - grazina nauja DataFrame
+# print(df)
+# df['Country'] = 'USA' # prideda nauja stulpeli su nurodyta reiksme
+# df['Country'] = ['USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'Lietuva'] # prideda nauja stulpeli su nurodyta reiksme, galima nurodyti skirtingas reiksmes skirtingiems eilutems
+# print(df)
+# df2 = df.copy() # copy - sukuria nauja DataFrame, kuris yra kopija originalo, pakeitimai viename neitakos kitam
+# df2.rename(columns={'Name': 'Full Name', 'Age': 'Age in Years'}, inplace=True) # rename - pervadina stulpelius, columns - nurodo kuriuos stulpelius pervadinti, inplace - pakeicia originala, jei False - grazina nauja DataFrame
+# df = pd.concat([df, df2], ignore_index=True) # concat - sujungia du DataFrame i viena, ignore_index - ignoruoja originalius indexus ir sukuria naujus nuo 0 iki n-1
+# print(df)
+df = pd.read_csv(r'data/titanic_train.csv') # read_csv - nuskaito duomenis is csv failo ir sukuria DataFrame
+print(df)
+# df.info()
+# pd.read_sql_query
+
+print(df.groupby('Sex')['PassengerId'].count()) # groupby - grupuoja duomenis pagal nurodyta stulpeli, count - suskaiciuoja kiek yra ne null reiksmiu kiekvienoje grupeje
+print(df.groupby('Pclass')['PassengerId'].count()) # groupby - grupuoja duomenis pagal nurodyta stulpeli, count - suskaiciuoja kiek yra ne null reiksmiu kiekvienoje grupeje
+print(df.groupby('Pclass')['Age'].mean()) # groupby - grupuoja duomen
